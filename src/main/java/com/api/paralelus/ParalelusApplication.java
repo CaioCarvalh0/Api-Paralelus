5acbce05debbdc84655be7b2fc3c1aaa85e96e14
Caio Oliveira <yinz.desenho@gmail.com>
2024-08-10 14:46:14 -0300

    firt commit

---
+++ src/main/java/com/api/paralelus/ParalelusApplication.java
@@ -0,0 +1,13 @@
+package com.api.paralelus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParalelusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParalelusApplication.class, args);
+	}
+
+}
