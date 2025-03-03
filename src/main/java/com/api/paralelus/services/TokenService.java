f4109596421d9df8717255579c32ed7e880dbefe
Caio Oliveira <yinz.desenho@gmail.com>
2025-03-03 19:58:40 -0300

    feat: Ajusta para passar gerar token random

--- src/main/java/com/api/paralelus/services/TokenService.java
+++ src/main/java/com/api/paralelus/services/TokenService.java
@@ -8,6 +8,8 @@ import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class TokenService {
 
@@ -20,6 +22,7 @@ public class TokenService {
             String token = JWT.create()
                     .withIssuer("API")
                     .withSubject(usuario.getLogin())
+                    .withJWTId(UUID.randomUUID().toString())
                     .sign(algorithm);
             return token;
         } catch (JWTCreationException e) {
