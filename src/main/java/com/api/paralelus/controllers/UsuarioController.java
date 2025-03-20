17c3d5a4d4cbb3fb52e3d7836938a96684885add
Caio Oliveira <yinz.desenho@gmail.com>
2025-03-20 20:40:32 -0300

    fix: Retira metodos para verificar usuario

--- src/main/java/com/api/paralelus/controllers/UsuarioController.java
+++ src/main/java/com/api/paralelus/controllers/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.api.paralelus.controllers;
 
+import com.api.paralelus.infra.security.ApiResponse;
 import com.api.paralelus.models.dto.UsuarioDTO;
 import com.api.paralelus.repository.UsuarioRepository;
 import jakarta.validation.Valid;
@@ -17,28 +18,6 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    @GetMapping("/checklogin")
-    public ResponseEntity checkLogin(@RequestBody @Valid UsuarioDTO data) {
-        if(this.usuarioRepository.findByLogin(data.login()) != null) {
-            return ResponseEntity.badRequest().build();
-        }
-        return ResponseEntity.ok().build();
-    }
 
-    @GetMapping("/checkemail")
-    public ResponseEntity checkEmail(@RequestBody @Valid UsuarioDTO data) {
-        if(this.usuarioRepository.findByEmail(data.email()) != null) {
-            return ResponseEntity.badRequest().build();
-        }
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping("/checkname")
-    public ResponseEntity checkName(@RequestBody @Valid UsuarioDTO data) {
-        if(this.usuarioRepository.findByNome(data.nome()) != null) {
-            return ResponseEntity.badRequest().build();
-        }
-        return ResponseEntity.ok().build();
-    }
 
 }
