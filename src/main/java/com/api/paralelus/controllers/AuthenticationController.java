b96e0fae49a4be4f41e2fe57b32eb30c3fda0389
Caio Oliveira <yinz.desenho@gmail.com>
2025-03-20 20:39:32 -0300

    feat: Ajusta login e cadastro para passar ApiResponse

--- src/main/java/com/api/paralelus/controllers/AuthenticationController.java
+++ src/main/java/com/api/paralelus/controllers/AuthenticationController.java
@@ -1,16 +1,21 @@
 package com.api.paralelus.controllers;
 
+import com.api.paralelus.infra.security.ApiResponse;
 import com.api.paralelus.models.dto.AuthenticationDTO;
 import com.api.paralelus.models.dto.LoginRespondeDTO;
 import com.api.paralelus.models.dto.RegisterDTO;
 import com.api.paralelus.models.Usuario;
+import com.api.paralelus.models.dto.UsuarioDTO;
 import com.api.paralelus.repository.UsuarioRepository;
 import com.api.paralelus.services.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,23 +34,41 @@ public class AuthenticationController {
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data) {
-        var user = new UsernamePasswordAuthenticationToken(data.login(), data.senha());
-        var auth = this.authenticationManager.authenticate(user);
-        var usuario = (Usuario) auth.getPrincipal();
-        var token = tokenService.generateToken(usuario);
-        return ResponseEntity.ok(new LoginRespondeDTO(token, usuario));
+        try {
+            var user = new UsernamePasswordAuthenticationToken(data.login(), data.senha());
+            var auth = this.authenticationManager.authenticate(user);
+            var usuario = (Usuario) auth.getPrincipal();
+            var token = tokenService.generateToken(usuario);
+            var response = new LoginRespondeDTO(token, usuario);
+            return ResponseEntity.ok().body(new ApiResponse(true, "Login efetuado com sucesso", response));
+        }catch (UsernameNotFoundException e) {
+            return ResponseEntity.ok().body(new ApiResponse(false, "Usuário não encontrado", null));
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.ok().body(new ApiResponse(false, "Senha inválida", null));
+        }
     }
 
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody @Valid RegisterDTO data) {
         if(this.usuarioRepository.findByLogin(data.login()) != null) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.ok(new ApiResponse(false, "Usuario Já Cadastrado", null));
         }
         String encryptedSenha = new BCryptPasswordEncoder().encode(data.senha());
         Usuario usuario = new Usuario(data.login(), data.nome(), data.email(), encryptedSenha);
         this.usuarioRepository.save(usuario);
 
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/checkcadastro")
+    public ResponseEntity<ApiResponse> checkCadastro(@RequestBody @Valid UsuarioDTO data) {
+        if(this.usuarioRepository.findByLogin(data.login()) != null) {
+            return ResponseEntity.ok(new ApiResponse(false, "Login invalido", null));
+        }
+        if(this.usuarioRepository.findByEmail(data.email()) != null) {
+            return ResponseEntity.ok(new ApiResponse(false, "Email invalido", null));
+        }
+        return ResponseEntity.ok(new ApiResponse(true, "", null));
     }
 
 }
